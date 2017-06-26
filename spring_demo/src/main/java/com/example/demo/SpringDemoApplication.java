1de1e3fb077573ed5eb64e2034972a7ef86303ff
RRADHAKRISHNAN <RRADHAKRISHNAN@L-AH786611.etslan.org>
2017-06-26 13:46:41 -0400

    Spring JPA feature enabled

--- spring_demo/src/main/java/com/example/demo/SpringDemoApplication.java
+++ spring_demo/src/main/java/com/example/demo/SpringDemoApplication.java
@@ -1,7 +1,16 @@
 package com.example.demo;
 
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringDemoApplication {
@@ -9,4 +18,55 @@ public class SpringDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
+}
+
+@Component
+class BookingCommandLineRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+interface BookingRepository extends JpaRepository<Booking, Long>{
+	
+	Collection<Booking> findByBookingName(String bookingName);
+}
+
+@Entity
+class Booking{
+	
+	@Id @GeneratedValue
+	Long id;
+	
+	String bookingName;
+
+	public Booking(String bookingName) {
+		super();
+		this.bookingName = bookingName;
+	}
+	
+	public Booking(){
+		
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getBookingName() {
+		return bookingName;
+	}
+
+	public void setBookingName(String bookingName) {
+		this.bookingName = bookingName;
+	}
+	
 }
---
+++ spring_demo/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into booking(booking_name) values('Kris');
+insert into booking(booking_name) values('Bob');
